b86a4876f91a58e865e6f484391cfa88cb706109
Pooja <pooja@gmail.com>
2024-08-23 18:31:32 +0530

    changed some files

--- src/test/java/stepDefinitions/StepDefinations.java
+++ src/test/java/stepDefinitions/StepDefinations.java
@@ -9,7 +9,7 @@ public class StepDefinations {
 	@Given("Add Place Payload")
 	public void add_place_payload() {
 	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+	    
 	}
 	@When("user calls {string} with Post http request")
 	public void user_calls_with_post_http_request(String string) {
